6e221190a1202f4e3129f118ec6a091dbc0e8a7b
Yush <yuhsih2@uci.edu>
2018-10-04 12:58:30 -0700

    Updated MyAI
    Able to uncover 8 neighbors next to start(X, Y)

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -14,6 +14,15 @@
 
 from AI import AI
 from Action import Action
+import numpy as np
+
+UNKNOWN = "."
+def _print(array):
+	print(np.flip(np.swapaxes(array, 0, 1), 0))
+
+# class Queue():
+# 	def __init__(self):
+# 		self.queue = []
 
 
 class MyAI( AI ):
@@ -23,7 +32,22 @@ class MyAI( AI ):
 		########################################################################
 		#							YOUR CODE BEGINS						   #
 		########################################################################
-		pass
+		# self.startX = startX
+		# self.startY = startY
+
+		self.rowDimension = rowDimension
+		self.colDimension = colDimension
+		self.array = np.array([[UNKNOWN for _ in range(colDimension)] for _ in range(rowDimension)])
+		# self.array[startX, startY] = 0
+		# self.move = [(3,3), (2,2), (1,1)]
+		self.x = startX
+		self.y = startY
+		self.startX = startX
+		self.startY = startY
+		self.array[self.x,self.y] = 0
+		self.q = []
+		self.q = self.addNeighbors(startX, startY)
+
 		########################################################################
 		#							YOUR CODE ENDS							   #
 		########################################################################
@@ -34,7 +58,69 @@ class MyAI( AI ):
 		########################################################################
 		#							YOUR CODE BEGINS						   #
 		########################################################################
+		self.array[self.x, self.y] = number
+		_print(self.array)
+
+		# print("(",self.x,",",self.y, ") neighbors: ",self.getNeighbors(self.x, self.y))
+		# print(self.addNeighbors(self.x, self.y))
+		"""
+		if uncover any 0
+		then get its all possible neighbors
+		add to queue
+		"""
+
+		# if number == 0:
+			# print(self.x, self.y)
+			# if (self.x, self.y) == (self.startX, self.startY):
+
+			# self.q = self.addNeighbors(self.x, self.y)
+			# self.addNeighbors(self.x, self.y)
+			# print(self.q)
+
+
+		'''
+		for now, this q is to uncover all neighbors around start cord 0
+		'''
+		if self.q:
+
+			self.x, self.y = self.q.pop()
+			print("NEXT CORD TO UNCOVER: (", self.x, ",", self.y, ")")
+			print("ALL OTHER POSSIBLE CORDS:", self.q)
+			return Action(AI.Action.UNCOVER, self.x, self.y)
+
+		"""
+		for testing purpose, this move is to uncover all cord listed in list move
+		"""
+		# if self.move:
+		# 	self.x, self.y = self.move.pop()
+		# 	print("NEXT CORD TO UNCOVER: (",self.x, ",", self.y, ")")
+		# 	return Action(AI.Action.UNCOVER, self.x, self.y)
+
+
+
 		return Action(AI.Action.LEAVE)
 		########################################################################
 		#							YOUR CODE ENDS							   #
 		########################################################################
+	def getNeighbors(self, a, b):
+		possibleN = [(a-1, b-1),(a-1, b),(a-1, b+1),\
+		       (a, b-1), (a, b+1), (a+1, b-1), (a+1, b), (a+1, b+1)]
+
+		return [(i,j) for i,j in possibleN if (i >=0 and i < self.colDimension and j >= 0 and j < self.rowDimension)]
+
+
+	def addNeighbors(self, a, b):
+		# for i,j in self.getNeighbors(self.x, self.y):
+		# 	if(i >= 0 and i < self.colDimension and j >= 0 and j < self.rowDimension):
+		# 		print(i,j)
+				# self.array[i,j] = "Q"
+		"""
+			get neighbors
+			if cord is not in queue, then add to queue
+			if already uncover or added in queue, don't add again
+		"""
+
+		for i,j in self.getNeighbors(a, b):
+			if (i,j) not in self.q:
+				self.q.append((i,j))
+		return self.q
