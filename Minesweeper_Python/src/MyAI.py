42a78c15e581d0b9a3dafe2dbfd1d7e0a001f182
Yush <yuhsih2@uci.edu>
2018-10-22 20:05:36 -0700

    change function to isExplored and organize some code

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -37,12 +37,12 @@ class MyAI( AI ):
 		self.array = np.array([[UNKNOWN for _ in range(colDimension)] for _ in range(rowDimension)])
 		# self.move = [(3,3), (2,2), (1,1)]
 		self.startX, self.startY = startX, startY
-		self.x, self.y = startX, startY
-		self.array[self.x, self.y] = 0
-		self.frontier = []
-		self.explored = []
-		self.flag = []
-		self.frontier = self.addNeighbors(startX, startY) #save all 8 neighbors to frontier
+		self.x, self.y = startX, startY                     #set (x,y) start from start (x,y)
+		self.array[self.x, self.y] = 0                      #display my world to debug
+		self.frontier = []                                  #ready to expand
+		self.explored = []                                  #already explored
+		self.flag = []                                      #these are bombs/mines
+		self.frontier = self.addNeighbors(startX, startY)   #add start(x,y) all possible neighbors to frontier list
 
 		########################################################################
 		#							YOUR CODE ENDS							   #
@@ -55,33 +55,29 @@ class MyAI( AI ):
 		#							YOUR CODE BEGINS						   #
 		########################################################################
 
-
-		'''
-		display my own world: array
-		'''
+		# Display my own world: array
+		# get cord(x,y) and its number
+		# add cord(x,y) to array
 		self.array[self.x, self.y] = number
 		print("MY WORLD:")
 		_print(self.array)
 
-		'''
-		add it to explored list after uncover it
-		'''
-		for i,j in self.addToExplored(self.x, self.y):
-			self.explored.append((i,j))
-		print("explored: ",self.explored)
+		# Add (x,y) to explored list after agent uncovered it
+		if not self.isExplored(self.x, self.y):
+			self.explored.append((self.x, self.y))
+		print("explored: ", self.explored)
 
-		'''
-		when uncover any tile with number 0, 
-		get all its possible neighbors and add them to frontier list
-		'''
+		# When agent uncovers any tile with number 0,
+		# get all its possible neighbors and add them to frontier list
 		if number == 0:
 			for i,j in self.addNeighbors(self.x, self.y):
 				self.frontier.append((i,j))
 		print("frontier:", self.frontier)
 
-		'''
-		Add to flag list and uncover the rest
-		'''
+
+		# Go through my world
+		# if numer == 1 and it has the same amount of hiddenTile as the number on the square
+		# Add (i,j) to flag list
 		for a in range(self.rowDimension):
 			for b in range(self.colDimension):
 				if self.array[a,b] == 1:
@@ -90,6 +86,9 @@ class MyAI( AI ):
 							self.flag.append((i,j))
 		print("flag:", self.flag)
 
+		# Go through my world
+		# if number == 1 and it has the same amount of flag as the number on the square
+		# uncover all rest of (i,j)
 		for a in range(self.rowDimension):
 			for b in range(self.colDimension):
 				if self.array[a,b] == 1:
@@ -97,30 +96,16 @@ class MyAI( AI ):
 						if (i,j) not in self.flag and (i,j) not in self.frontier:
 							self.frontier.append((i,j))
 
-		# for i,j in self.addToFlag(2,2):
-		# 	print("should add to flag list:", (i,j))
-		# 	if (i,j) not in self.flag:
-		# 		self.flag.append((i,j))
-		# print("flag:", self.flag)
 
-
-
-
-
-		'''
-		if frontier list is not empty, pop it to uncover
-		for now, i can pop all neighbors adjacent to (startX,startY)
-		'''
+		# if frontier list is not empty, pop it as (x,y) to uncover
 		if self.frontier:
-
 			self.x, self.y = self.frontier.pop()
 			print("NEXT CORD TO UNCOVER: (", self.x, ",", self.y, ")")
 			print("POP (", self.x, ",", self.y, ")", "ALL OTHER POSSIBLE TILES IN FRONTIER:", self.frontier)
 			return Action(AI.Action.UNCOVER, self.x, self.y)
 
-		"""
-		just testing, this move is to uncover all cord listed in the list called move
-		"""
+
+		#just testing, this move is to uncover all cord listed in the list called move
 		# if self.move:
 		# 	self.x, self.y = self.move.pop()
 		# 	print("NEXT CORD TO UNCOVER: (",self.x, ",", self.y, ")")
@@ -131,7 +116,9 @@ class MyAI( AI ):
 		#							YOUR CODE ENDS							   #
 		########################################################################
 	'''
-	given (a,b), get its all adjacent neighbors, 8 possible neighbors
+	function: getNeighbors
+	input: given (a,b)
+	output: return a list of (a,b)'s all possible neighbors, max: 8 neighbors
 	'''
 	def getNeighbors(self, a, b):
 		possibleN = [(a-1, b-1),(a-1, b),(a-1, b+1),\
@@ -140,7 +127,9 @@ class MyAI( AI ):
 		return [(i,j) for i,j in possibleN if (i >=0 and i < self.colDimension and j >= 0 and j < self.rowDimension)]
 
 	'''
-	given (a,b), add its all neighbors to a list
+	function: addNeighbors
+	input: given (a,b)
+	output: return a list of (a,b)'s neighbors (filter: not in frontier list and not in explored list)
 	'''
 	def addNeighbors(self, a, b):
 		# for i,j in self.getNeighbors(self.x, self.y):
@@ -156,20 +145,21 @@ class MyAI( AI ):
 
 		L = []
 		for i,j in self.getNeighbors(a, b):
-			# if (i,j) not in self.frontier and (i,j) != (self.startX, self.startY):
 			if (i,j) not in self.frontier and (i,j) not in self.explored:
 				L.append((i,j))
 		return L
 
 	'''
-	given (a,b), if it's not explored yet, then add it to explored after uncover it
+	function: isExplored 
+		if it's not explored yet, then add it to explored after uncover it
+	input: given (a,b)
+	output: return true(if already explored)/false(if not explored yet)
 	'''
-	def addToExplored(self, a, b):
-
-		L = []
+	def isExplored(self, a, b):
 		if (a,b) not in self.explored:
-			L.append((a,b))
-		return L
+			return False
+		else: True
+
 
 	'''
 	for now, only deal with number 1
@@ -181,6 +171,11 @@ class MyAI( AI ):
 	# logic: if a tile has the same amount of flags around it as the number on the square
 	#        then all remaining hidden tiles around it are not bombs
 
+	'''
+	function: addToFlag
+	input: given (a,b)
+	output: return a list (either empty or a list of HiddenTile)
+	'''
 	def addToFlag(self, a, b):
 		numOfHiddenTile = 0
 		HiddenTile = []
@@ -194,7 +189,11 @@ class MyAI( AI ):
 		else:
 			return []
 
-
+	'''
+	function: addToUncover
+	input: given (a,b)
+	output: return a list (either empty or a list of HiddenTile)
+	'''
 	def addToUncover(self, a, b):
 		numOfFlag = 0
 		numOfHiddenTile = 0
