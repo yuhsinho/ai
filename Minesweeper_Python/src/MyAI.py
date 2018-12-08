b7cc371dd845a57ad93100aea0989c6e020dbbc3
Yush <yuhsih2@uci.edu>
2018-12-08 13:30:12 -0800

    Rename functions and created some general small function
    check_explored, num_flag, num_unrevealed, get_unrevealed,  and now Beginner 2 min, Intermediate 55min

--- Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Python/src/MyAI.py
@@ -46,7 +46,7 @@ class MyAI( AI ):
 		self.flag = []
 
 		# add R_x,y (as the set of all squares adjacent to B_x,y) to frontier list
-		self.frontier = self.addNeighbors(startX, startY)
+		self.frontier = self.get_new_neighbors(startX, startY)
 
 		########################################################################
 		#							YOUR CODE ENDS							   #
@@ -63,40 +63,36 @@ class MyAI( AI ):
 		self.B[self.x, self.y] = number
 
 		# Display my board B
-		print("MY BOARD B:")
-		_print(self.B)
+		# _print(self.B)
 
 		# Check if B_x,y is explored. If not, add B_x,y to explored list
-		for i,j in self.isExplored(self.x, self.y):
-			self.explored.append((i,j))
-		print("Explored list: ", self.explored)
+		if self.not_explored_yet(self.x, self.y):
+			self.explored.append((self.x, self.y))
 
 		# if K(B_x,y) == 0, add R_x,y to frontier list
 		if number == 0:
-			for i,j in self.addNeighbors(self.x, self.y):
+			for i,j in self.get_new_neighbors(self.x, self.y):
 				self.frontier.append((i,j))
 
 		# apply theorems 1 and 2
 		while True:
 			applied_theorem = False
-			# for i in range(self.colDimension):
-			# 	for j in range(self.rowDimension):
 			for i,j in self.explored:
 					applied_theorem |= self.apply_theorems(i,j)
 			if not applied_theorem:
 				break
 
-
 		if self.frontier:
 			self.x, self.y = self.frontier.pop()
-			print("FLAG LIST: ", self.flag)
-			print("NEXT CORD TO UNCOVER: (", self.x, ",", self.y, ")")
-			print("POP (", self.x, ",", self.y, ")")
-			print("ALL OTHER POSSIBLE TILES IN FRONTIER:", self.frontier)
+			# print("Explored:", self.explored)
+			# print("FLAG LIST: ", self.flag)
+			# print("NEXT CORD TO UNCOVER: (", self.x, ",", self.y, ")")
+			# print("POP (", self.x, ",", self.y, ")")
+			# print("ALL OTHER POSSIBLE TILES IN FRONTIER:", self.frontier)
 			return Action(AI.Action.UNCOVER, self.x, self.y)
 
-		# CSP or Equation Strategy
-		all_cons = self.all_constraints()
+		# Equation Strategy
+		all_cons = self.get_all_constraints()
 		self.equation_difference(all_cons)
 		safe = []
 		bomb = []
@@ -115,28 +111,128 @@ class MyAI( AI ):
 				self.flag.append((i,j))
 
 		# If current_total_flag == total_mines, add the rest unrevealed squares to frontier list
-		if self.isCompleted():
-			for i, j in self.getUnknownSquares():
+		if self.is_completed():
+			for i, j in self.get_all_unknown_squares():
 				self.frontier.append((i, j))
 
 		if self.frontier:
 			self.x, self.y = self.frontier.pop()
-			print("FLAG LIST: ", self.flag)
-			print("NEXT CORD TO UNCOVER: (", self.x, ",", self.y, ")")
-			print("POP (", self.x, ",", self.y, ")")
-			print("ALL OTHER POSSIBLE TILES IN FRONTIER:", self.frontier)
 			return Action(AI.Action.UNCOVER, self.x, self.y)
 
 		# Random choice
-		unknown_squares = self.getUnknownSquares()
+		unknown_squares = self.get_all_unknown_squares()
 		if unknown_squares:
-			self.x, self.y = random.choice(self.getUnknownSquares())
+			self.x, self.y = random.choice(self.get_all_unknown_squares())
 			return Action(AI.Action.UNCOVER, self.x, self.y)
 
 		return Action(AI.Action.LEAVE)
 		########################################################################
 		#							YOUR CODE ENDS							   #
 		########################################################################
+	'''
+	function: get_neighbors(a, b)
+	input: given coordinate(a,b)
+	output: return a list of (a,b)'s all adjacent neighbors
+	'''
+	def get_neighbors(self, a, b):
+		neighbors = [(a-1, b-1),(a-1, b),(a-1, b+1),\
+		       (a, b-1), (a, b+1), (a+1, b-1), (a+1, b), (a+1, b+1)]
+		return [(i,j) for i,j in neighbors if (i >=0 and i < self.colDimension and j >= 0 and j < self.rowDimension)]
+
+	'''
+	function: get_new_neighbors(a, b)
+	input: given coordinate(a,b)
+	output: return a list of (a,b)'s adjacent neighbors (filter: not in frontier list and not in explored list)
+	'''
+	def get_new_neighbors(self, a, b):
+		L = []
+		for i,j in self.get_neighbors(a, b):
+			if (i,j) not in self.frontier and (i,j) not in self.explored:
+				L.append((i,j))
+		return L
+
+	'''
+	function: not_explored_yet(a, b)	
+	input: given coordinate(a,b)
+	output: return True if not explored yet 
+	'''
+	def not_explored_yet(self, a, b):
+		if (a,b) not in self.explored:
+			return True
+
+	'''
+	function: get_num_of_flag(a,b)
+	input: given a cord(a,b)
+	output: a number of flag in the flag list
+	'''
+	def get_num_of_flag(self, a, b):
+		num = 0
+		for i,j in self.get_neighbors(a,b):
+			if (i,j) in self.flag:
+				num += 1
+		return num
+
+	'''
+	function: get_num_of_unrevealed(a,b)
+	input: given a cord(a,b)
+	output: a number of unrevealed squares
+	'''
+	def get_num_of_unrevearled(self, a, b):
+		num = 0
+		for i,j in self.get_neighbors(a,b):
+			if (i,j) not in self.explored and (i,j) not in self.flag:
+				num += 1
+		return num
+
+	'''
+	function: get_unrevealed(a,b)
+	input: given a cord(a,b)
+	output: a list of unrevealed squares
+	'''
+	def get_unrevealed(self, a,b ):
+		L = []
+		for i,j in self.get_neighbors(a,b):
+			if (i,j) not in self.explored and (i,j) not in self.flag:
+				L.append((i,j))
+		return L
+	'''
+	Theorem 1: 
+	Given a board B and square B_x,y, 
+	if # of unrevealed squares in R_x,y == K(B_x,y) (minus # of flagged squares in R_x,y),
+	then all of those unrevealed squares contain a bomb.
+	
+	function: theorem1(a, b, value)
+	input: given coordinate(a, b)
+	output: return a list of unrevealed squares (ready to be flagged)
+	'''
+	def theroem1(self, a, b, value):
+		num_of_unrevealed = self.get_num_of_unrevearled(a,b)
+		num_of_flag = self.get_num_of_flag(a,b)
+		unrevealed_squares = []
+		if num_of_unrevealed == (value - num_of_flag):
+			for i,j in self.get_unrevealed(a,b):
+				unrevealed_squares.append((i,j))
+		return unrevealed_squares
+
+	'''
+	Theorem 2:
+	Given a board B and square B_x,y,
+	if # of flagged squares surrounding B_x,y == K(B_x,y),
+	every unrevealed square in R_x,y does not contain a bomb.
+	
+	function: theorem2(a, b, value)
+	input: given coordinate(a,b)
+	output: return a list of unrevealed squares (ready to uncover, so filter those who are already in frontier list)
+	'''
+	def theorem2(self, a, b, value):
+		num_of_flag = self.get_num_of_flag(a,b)
+		unrevealed_squares = []
+		if num_of_flag == value:
+			for i,j in self.get_unrevealed(a,b):
+				if (i,j) not in self.frontier:
+					unrevealed_squares.append((i,j))
+		return unrevealed_squares
+
 	'''
 	function: apply_theorems(a,b)
 	input: given coordinate(a,b)
@@ -147,148 +243,64 @@ class MyAI( AI ):
 		if x == -1:
 			return False
 		#theorem 1
-		list_of_bomb_cords = self.addToFlag(a,b,x)
+		list_of_bomb_cords = self.theroem1(a,b,x)
 		applied_theorem1 = bool(list_of_bomb_cords)
 		for i,j in list_of_bomb_cords:
 			self.flag.append((i,j))
 		#theorem 2
-		list_of_safe_unrevealed_cords = self.addToUncover(a,b,x)
+		list_of_safe_unrevealed_cords = self.theorem2(a,b,x)
 		applied_theorem2 = bool(list_of_safe_unrevealed_cords)
 		for i,j in list_of_safe_unrevealed_cords:
 			self.frontier.append((i,j))
 		return applied_theorem1 or applied_theorem2
 
 	'''
-	function: getNeighbors(a, b)
-	input: given coordinate(a,b)
-	output: return a list of (a,b)'s all adjacent neighbors
-	'''
-	def getNeighbors(self, a, b):
-		possibleN = [(a-1, b-1),(a-1, b),(a-1, b+1),\
-		       (a, b-1), (a, b+1), (a+1, b-1), (a+1, b), (a+1, b+1)]
-
-		return [(i,j) for i,j in possibleN if (i >=0 and i < self.colDimension and j >= 0 and j < self.rowDimension)]
-
-	'''
-	function: addNeighbors(a, b)
-	input: given coordinate(a,b)
-	output: return a list of (a,b)'s adjacent neighbors (filter: not in frontier list and not in explored list)
-	'''
-	def addNeighbors(self, a, b):
-		L = []
-		for i,j in self.getNeighbors(a, b):
-			if (i,j) not in self.frontier and (i,j) not in self.explored:
-				L.append((i,j))
-		return L
-
-	'''
-	function: isExplored(a, b)	
-	input: given coordinate(a,b)
-	output: return (a,b) if it's not explored, return empty list if it's explored
-	--> not sure why boolean dont work that well
-	'''
-	def isExplored(self, a, b):
-		L = []
-		if (a,b) not in self.explored:
-			L.append((a,b))
-		return L
-
-	'''
-	Theorem 1: 
-	Given a board B and square B_x,y, 
-	if # of unrevealed squares in R_x,y == K(B_x,y) (minus # of flagged squares in R_x,y),
-	then all of those unrevealed squares contain a bomb.
-	
-	function: addToFlag(a, b, num)
-	input: given coordinate(a, b)
-	output: return a list (either empty or a list of UnrevealedSquares)
-	'''
-	def addToFlag(self, a, b, num):
-		numOfUnrevealed = 0
-		numOfFlag = 0
-		UnrevealedSquares = []
-		for i,j in self.getNeighbors(a,b):
-			if (i,j) in self.flag:
-				numOfFlag += 1
-			if (i,j) not in self.explored and (i,j) not in self.flag:
-				numOfUnrevealed +=1
-				UnrevealedSquares.append((i,j))
-
-		if numOfUnrevealed == (num - numOfFlag):
-			return UnrevealedSquares
-		else: return []
-
-	'''
-	Theorem 2:
-	Given a board B and square B_x,y,
-	if # of flagged squares surrounding B_x,y == K(B_x,y),
-	every unrevealed square in R_x,y does not contain a bomb.
-	
-	function: addToUncover(a, b, num)
-	input: given coordinate(a,b)
-	output: return a list (either empty or a list of UnrevealedSquares)
-	'''
-	def addToUncover(self, a, b, num):
-		numOfFlag = 0
-		UnrevealedSquares = []
-		for i,j in self.getNeighbors(a,b):
-			if (i,j) in self.flag:
-				numOfFlag += 1
-			if (i,j) not in self.explored and (i,j) not in self.flag and (i,j) not in self.frontier:
-				UnrevealedSquares.append((i,j))
-
-		if numOfFlag == num:
-			return UnrevealedSquares
-		else:
-			return []
-
-	'''
-	function: TotalFlag()
+	function: total_flag()
 	input: None
-	output: return total number of flags)
+	output: return total number of flags in board B
 	'''
-	def TotalFlag(self):
-		numOfFlag = 0
+	def total_flag(self):
+		num = 0
 		for i, j in self.flag:
-			numOfFlag += 1
-		return numOfFlag
+			num += 1
+		return num
 
 	'''
-	function: isCompleted()
+	function: is_completed()
 	input: None
-	output: return true/false
+	output: return true or false
 	# If TotalFlag == totalMines, add the rest unrevealed squares to frontier list
 	'''
-	def isCompleted(self):
-		if self.totalMines == self.TotalFlag():
+	def is_completed(self):
+		if self.totalMines == self.total_flag():
 			return True
-		# else: return False
 
 	'''
-	function: getUnknownSquares() (define B_U as the set of all unknown squares)
+	function: get_all_unknown_squares() (define B_U as the set of all unknown squares)
 	input: None
-	output: return a list of unknown squares
+	output: return a list of all unknown squares in board B
 	'''
-	def getUnknownSquares(self):
+	def get_all_unknown_squares(self):
 		L = []
 		for a in range(self.colDimension):
 			for b in range(self.rowDimension):
-				if self.B[a,b] == -1 and (a,b) not in self.flag and (a,b) not in self.frontier:
+				# if (a,b) not in self.explored and (a,b) not in self.flag and (a,b) not in self.frontier:
+				if self.B[a, b] == -1 and (a, b) not in self.flag and (a, b) not in self.frontier:
 					L.append((a,b))
 		return L
 
 	'''
 	function: get_perimeter()
-	(define P_B as the set of unknown squares in B that are adjacent to any revealed numbered square)
+	(define PB as the set of unknown squares in B that are adjacent to any revealed numbered square)
 	input: none
-	output: return a list of P_B
+	output: return a list of PB
 	'''
 	def get_perimeter(self):
 		L = []
-		for a, b in self.getUnknownSquares():
-			for i,j in self.getNeighbors(a,b):
-				# revealed numbered square: explored, not in flag list?!
-				if self.B[i,j] != -1 and (a,b) not in L and (i,j) not in self.flag:
+		for a, b in self.get_all_unknown_squares():
+			for i,j in self.get_neighbors(a,b):
+				# if (i,j) in self.explored and (a,b) not in L:
+				if self.B[i, j] != -1 and (a, b) not in L and (i, j) not in self.flag:
 					L.append((a,b))
 		return L
 
@@ -300,56 +312,50 @@ class MyAI( AI ):
 	def get_revealed_adjacent_to_p(self):
 		L = []
 		for a, b in self.get_perimeter():
-			for i, j in self.getNeighbors(a,b):
-				# revealed numbered square: explored, not in flag list
-				if self.B[i,j] != -1 and (i,j) not in L and (i,j) not in self.flag:
+			for i, j in self.get_neighbors(a,b):
+				# if (i,j) in self.explored and (i,j) not in L:
+				if self.B[i, j] != -1 and (i, j) not in L and (i, j) not in self.flag:
 					L.append((i,j))
 		return L
 
 	'''
-	function: get_constraint_variables()
-	input: given a cord(a,b) (a revealed square adjacent to p)
-	output: return a list of constraint variables(perimeters) vi = [ p1, p2,..]
+	function: get_constraint_variables(a,b)
+	input: given a cord(a,b), named vi: a revealed square adjacent to PB 
+	output: return a list of perimeters adjacent to vi, vi = [ p1, p2,..]
 	'''
 	def get_constraint_variables(self, a, b):
-		perimeter_list = self.get_perimeter()
 		L = []
-		for i, j in perimeter_list:
-			if (i,j) in self.getNeighbors(a,b):
+		for i, j in self.get_perimeter():
+			if (i,j) in self.get_neighbors(a,b):
 				L.append((i,j))
 		return L
 
 	'''
-	function: get_constraints()
-	input: given a cord(a,b) ( a revealed square adjacent to p)
-	output: return a list of constraints ci = [ [v], sum ]
-	after go through each (a,b), it will be c = [ [ [v1], sum1 ], [ [v2], sum2 ], ...] 
+	function: get_constraint(a,b)
+	input: given a cord(a,b), named vi: a revealed square adjacent to p
+	output: return a list of constraint, named ci = [ [vi], sumi ]	
 	'''
-	def get_constraints(self, a, b):
-		bomb = 0
-		for i,j in self.getNeighbors(a,b):
-			if (i,j) in self.flag:
-				bomb += 1
-		sum = self.B[a,b] - bomb
+	def get_constraint(self, a, b):
+		num_of_flag = self.get_num_of_flag(a,b)
+		sum = self.B[a,b] - num_of_flag
 		L = []
 		L.append([self.get_constraint_variables(a,b),sum])
 		return L
 
 	'''
-		function: all_contraints()
-		input: none
-		output: a list of contraints c = [ [ [v1], sum1 ], [ [v2], sum2 ], ...] 
-		'''
-
-	def all_constraints(self):
-		cons = []
+	function: get_all_contraints()
+	input: none
+	output: a list of constraints c = [c1,c2,..] = [ [ [v1], sum1 ], [ [v2], sum2 ], ...] 
+	'''
+	def get_all_constraints(self):
+		L = []
 		for i, j in self.get_revealed_adjacent_to_p():
-			for c in self.get_constraints(i, j):
-				cons.append(c)
-		return cons
+			for c in self.get_constraint(i, j):
+				L.append(c)
+		return L
 
 	'''
-	function: _print_csp(csp)
+	function: _print_csp(csp) (for testing purpose)
 	input: a list of constraints c = [ [ [v1], sum1 ], [ [v2], sum2 ], ...] 
 	output: simple format to check the list
 	'''
@@ -359,7 +365,7 @@ class MyAI( AI ):
 
 	'''
 	function: equation_difference(csp)
-	input: a list of contraints c = [ [ [v1], sum1 ], [ [v2], sum2 ], ...] 
+	input: a list of constraints c = [ [ [v1], sum1 ], [ [v2], sum2 ], ...] 
 	output: a list of updated constraints
 	'''
 	def equation_difference(self, csp):
@@ -376,8 +382,6 @@ class MyAI( AI ):
 					elif len(con1[0]) > len(con0[0]):
 						con1[0] = list(set(con1[0]).difference(set(con0[0])))
 						con1[1] = con1[1] - con0[1]
-					# when we are in this if statement, that means ci is updated
-					# once ci is updated, repeat the function again
 					self.equation_difference(csp)
 		return csp
 
@@ -390,35 +394,29 @@ class MyAI( AI ):
 		update_list = []
 		for i in range(len(csp)):
 			if len(csp[i][0]) == 1:
-				# [ [p1], 0 ]
 				update_list.append(csp[i])
-
-			# not sure if necessary
-			elif csp[i][1] == 0:
-				# [ [p1,p2,p3], 0]
-				update_list.append(csp[i])
-
+			# if csp[i][1] == 0:
+			# 	update_list.append(csp[i])
 		return update_list
 
-	def get_unknown_without_p(self):
-		L = []
-		for i,j in self.getUnknownSquares():
-			if (i,j) not in self.get_perimeter():
-				L.append((i,j))
-		return L
-
-
-	# def update_c(self,csp):
+	# def find_not_update_tiles(self, csp):
 	# 	L = []
-	# 	for i in self.find_update_tiles(csp):
-	# 		for a,b in i[0]:
-	# 			L.append((a,b))
+	# 	for i in range(len(csp)):
+	# 		if len(csp[i][0]) != 1:
+	# 			L.append(csp[i])
 	# 	return L
 
-	# def guess_corner(self):
-	# 	L = []
-	# 	corner = [(0,0), (0,self.colDimension-1), (self.rowDimension-1,0),(self.rowDimension-1, self.colDimension-1)]
-	# 	for i,j in corner:
-	# 		if (i,j) not in self.flag and (i,j) not in self.explored:
-	# 			L.append((i,j))
-	# 	return L
+	def find_fifty(self, csp):
+		list = []
+		for i in range(len(csp)):
+			if len(csp[i][0]) == 2 and csp[i][1] == 1:
+				list.append(csp[i])
+		return list
+
+	def random_choice(self, list):
+		a = random.choice(list)
+		return a
+	# def guess_list(self, csp):
+	# 	cons = self.find_fifty(csp)
+	# 	for i in range(len(csp)):
+	# 		csp[i][0]
